1096af25b8d7bfb0bb48fb0438bb1fb48d371467
Serhii Kovalenko <skovalenko@exadel.com>
2021-12-22 16:03:06 +0200

    [EQKP-8] Accept QueryBuilder query syntax

--- core/src/main/java/com/exadel/etoolbox/query/core/services/impl/QueryConverterServiceImpl.java
+++ core/src/main/java/com/exadel/etoolbox/query/core/services/impl/QueryConverterServiceImpl.java
@@ -1,42 +1,61 @@
 package com.exadel.etoolbox.query.core.services.impl;
 
+import com.day.cq.search.PredicateConverter;
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.text.Text;
 import com.exadel.etoolbox.query.core.services.QueryConverterService;
 import com.exadel.etoolbox.query.core.servlets.model.QueryResultModel;
+import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jackrabbit.commons.query.sql2.Parser;
 import org.apache.jackrabbit.oak.query.xpath.XPathToSQL2Converter;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.jcr.RepositoryException;
 import javax.jcr.Session;
-import javax.jcr.UnsupportedRepositoryOperationException;
 import javax.jcr.ValueFactory;
 import javax.jcr.query.qom.QueryObjectModel;
 import javax.jcr.query.qom.QueryObjectModelFactory;
+import java.io.IOException;
+import java.io.StringReader;
 import java.text.ParseException;
+import java.util.Properties;
 
 @Component(service = QueryConverterService.class)
 public class QueryConverterServiceImpl implements QueryConverterService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryConverterServiceImpl.class);
+
     private static final String X_PATH_LANGUAGE = "XPath";
+    private static final String QUERY_BUILDER_LANGUAGE = "queryBuilder";
+
+    @Reference
+    private QueryBuilder queryBuilder;
 
     @Override
     public QueryObjectModel convertQueryToJQOM(ResourceResolver resourceResolver, QueryResultModel queryResultModel) {
         String query = queryResultModel.getQuery();
         try {
-            if (queryResultModel.getLanguage().equals(X_PATH_LANGUAGE)) {
-                XPathToSQL2Converter converter = new XPathToSQL2Converter();
-                query = converter.convert(queryResultModel.getQuery());
-            }
             Session session = resourceResolver.adaptTo(Session.class);
             if (session == null) {
                 return null;
+            }
+
+            if (queryResultModel.getLanguage().equals(X_PATH_LANGUAGE)) {
+                query = convertXPathToSql2Query(queryResultModel.getQuery());
+            } else if (queryResultModel.getLanguage().equals(QUERY_BUILDER_LANGUAGE)) {
+                query = convertQueryBuilderQueryToSql2(queryResultModel.getQuery(), session);
             }
             QueryObjectModelFactory qomFactory = session.getWorkspace().getQueryManager().getQOMFactory();
             ValueFactory valueFactory = session.getValueFactory();
             Parser parser = new Parser(qomFactory, valueFactory);
             QueryObjectModel queryObjectModel = parser.createQueryObjectModel(query);
-            queryResultModel.setResultCount(queryObjectModel.execute().getRows().getSize());
+            queryResultModel.setResultCount(IteratorUtils.size(queryObjectModel.execute().getRows()));
             if (queryResultModel.getLimit() != null) {
                 queryObjectModel.setLimit(queryResultModel.getLimit());
             }
@@ -44,13 +63,37 @@ public class QueryConverterServiceImpl implements QueryConverterService {
                 queryObjectModel.setOffset(queryResultModel.getOffset());
             }
             return queryObjectModel;
-        } catch (UnsupportedRepositoryOperationException e) {
-            e.printStackTrace();
-        } catch (RepositoryException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            LOGGER.warn("Cannot create queryObjectModel", e);
         }
         return null;
+    }
+
+    private String convertQueryBuilderQueryToSql2(String query, Session session) throws ParseException, IOException {
+        Properties props = convertToProperties(query);
+        PredicateGroup root = PredicateConverter.createPredicates(props);
+        // avoid slow //* queries
+        if (root.isEmpty()) {
+            LOGGER.debug("Predicate group cannot be empty");
+            return null;
+        }
+
+        Query queryBuilderQuery = queryBuilder.createQuery(root, session);
+        queryBuilderQuery.setHitsPerPage(0);
+        return convertXPathToSql2Query(queryBuilderQuery.getResult().getQueryStatement());
+    }
+
+    private String convertXPathToSql2Query(String xPathQuery) throws ParseException {
+        XPathToSQL2Converter converter = new XPathToSQL2Converter();
+        String sql2 = converter.convert(xPathQuery);
+        return StringUtils.substringBefore(sql2, " /* xpath: ");
+    }
+
+    private Properties convertToProperties(String query) throws IOException {
+        Properties properties = new Properties();
+        try (StringReader reader = new StringReader(Text.unescape(StringUtils.replace(query, "&", "\n")))) {
+            properties.load(reader);
+            return properties;
+        }
     }
 }
