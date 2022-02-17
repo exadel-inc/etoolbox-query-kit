d08febad220319a892c9947cfdc6d3bf725e0f0d
nharshunova <nharshunova@exadel.com>
2022-02-17 19:12:48 +0300

    [TECH] es6 activate, add codemirror & eslint via npm

--- ui.apps/src/main/content/jcr_root/apps/etoolbox-query-kit/components/content/console/clientlibs/js/share-query.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-query-kit/components/content/console/clientlibs/js/share-query.js
@@ -1,68 +1,67 @@
-"use strict"
+'use strict';
 
 $(document).ready(function () {
+    const $shareButton = $('#shareButton');
+    const $queryForm = $('#queryForm');
+    const $executeButton = $('#executeButton');
 
-    var $shareButton = $('#shareButton'),
-        $queryForm = $('#queryForm'),
-        $executeButton = $('#executeButton');
-
-    var successTooltip = new Coral.Tooltip().set({
-      content: {
-        innerHTML: 'URL copied to clipboard'
-      },
-      variant: 'success',
-      target: '#shareButton',
-      placement: 'bottom',
-      interaction: 'off'
+    const successTooltip = new Coral.Tooltip().set({
+        content: {
+            innerHTML: 'URL copied to clipboard'
+        },
+        variant: 'success',
+        target: '#shareButton',
+        placement: 'bottom',
+        interaction: 'off'
     });
 
     $shareButton.append(successTooltip);
 
     function updateQueryFromUrl() {
-        var urlParams = decodeQueryUrlParams();
-        var editor = document.querySelector('.CodeMirror').CodeMirror;
-        if (urlParams.query && urlParams.query.trim().length > 0){
-           editor.setValue(urlParams.query);
+        const urlParams = decodeQueryUrlParams();
+        const editor = document.querySelector('.CodeMirror').CodeMirror;
+        if (urlParams.query && urlParams.query.trim().length > 0) {
+            editor.setValue(urlParams.query);
         }
     }
 
-   function decodeQueryUrlParams() {
-       var urlParameters = getUrlParameters();
-       return {
-            query: urlParameters["query"]
-       }
-   };
+    function decodeQueryUrlParams() {
+        const urlParameters = getUrlParameters();
+        return {
+            query: urlParameters.query
+        };
+    };
 
     function getUrlParameters() {
-        var urlParameters = {};
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i = 0; i < vars.length; i++) {
-          var pair = vars[i].split("=");
-          try {
-            urlParameters[pair[0]] = decodeURIComponent(pair[1]);
-          } catch (e) {
-            console.log("Param of url is not correct.");
-            urlParameters[pair[0]] = "";
-          }
+        const urlParameters = {};
+        const query = window.location.search.substring(1);
+        const vars = query.split('&');
+        for (let i = 0; i < vars.length; i++) {
+            const pair = vars[i].split('=');
+            try {
+                urlParameters[pair[0]] = decodeURIComponent(pair[1]);
+            } catch (e) {
+                console.log('Param of url is not correct.');
+                urlParameters[pair[0]] = '';
+            }
         }
         return urlParameters;
     }
 
-    $shareButton.on('click', (function () {
-        var editor = document.querySelector('.CodeMirror').CodeMirror;
-        var query = editor.getValue();
+    $shareButton.on('click', function () {
+        const editor = document.querySelector('.CodeMirror').CodeMirror;
+        const query = editor.getValue();
         if (query) {
             successTooltip.open = true;
-            var urlWithoutParams = window.location.origin + window.location.pathname;
-            navigator.clipboard.writeText(urlWithoutParams  + '?query=' + encodeURIComponent(query));
-            setTimeout(function(){
+            const urlWithoutParams = window.location.origin + window.location.pathname;
+            navigator.clipboard.writeText(urlWithoutParams + '?query=' + encodeURIComponent(query));
+            setTimeout(function () {
                 successTooltip.open = false;
-            }, 2000)
+            }, 2000);
         }
-    }));
+    });
 
-    setTimeout(function() {
-      updateQueryFromUrl();
+    setTimeout(function () {
+        updateQueryFromUrl();
     }, 0);
 });
