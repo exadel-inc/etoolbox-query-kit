84fcd942332628297f99980c8314579f29955ee3
Stepan Miakchilo <smiakchilo@exadel.com>
2022-02-18 11:19:59 +0300

    [Tech] Renamed ParseException to ConverterException

---
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/utils/ConverterException.java
@@ -0,0 +1,12 @@
+package com.exadel.etoolbox.querykit.core.utils;
+
+public class ConverterException extends Exception {
+
+    public ConverterException(String message) {
+        super(message);
+    }
+
+    public ConverterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
