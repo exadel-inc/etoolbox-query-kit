54bd48f032a140a73698be25ccc9f26cc4416e02
Stepan Miakchilo <smiakchilo@exadel.com>
2022-02-18 11:42:29 +0300

    [Tech] Implemented MeasuredQueryResult

---
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/models/queryengine/MeasuredQueryResult.java
@@ -0,0 +1,40 @@
+package com.exadel.etoolbox.querykit.core.models.queryengine;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.query.QueryResult;
+import javax.jcr.query.RowIterator;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class MeasuredQueryResult implements QueryResult {
+
+    private final QueryResult original;
+
+    @Getter
+    private long total;
+
+    @Override
+    public String[] getColumnNames() throws RepositoryException {
+        return original.getColumnNames();
+    }
+
+    @Override
+    public RowIterator getRows() throws RepositoryException {
+        return original.getRows();
+    }
+
+    @Override
+    public NodeIterator getNodes() throws RepositoryException {
+        return original.getNodes();
+    }
+
+    @Override
+    public String[] getSelectorNames() throws RepositoryException {
+        return original.getSelectorNames();
+    }
+}
