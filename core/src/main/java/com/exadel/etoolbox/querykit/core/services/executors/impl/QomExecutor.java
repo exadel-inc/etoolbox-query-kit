55a7c8d88b2cf643b5340d24b4055fbbe2b04019
Stepan Miakchilo <smiakchilo@exadel.com>
2022-02-19 14:10:21 +0300

    [Tech] Added user vars interpolation in basic query executors when called from QueryService

--- core/src/main/java/com/exadel/etoolbox/querykit/core/services/executors/impl/QomExecutor.java
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/services/executors/impl/QomExecutor.java
@@ -3,6 +3,9 @@ package com.exadel.etoolbox.querykit.core.services.executors.impl;
 import com.exadel.etoolbox.querykit.core.models.qom.QomAdapter;
 import com.exadel.etoolbox.querykit.core.models.qom.columns.ColumnCollection;
 import com.exadel.etoolbox.querykit.core.models.qom.columns.ModifiableColumnCollection;
+import com.exadel.etoolbox.querykit.core.models.qom.constraints.ConstraintAdapter;
+import com.exadel.etoolbox.querykit.core.models.qom.constraints.LiteralHolder;
+import com.exadel.etoolbox.querykit.core.models.qom.constraints.helpers.InterpolationHelper;
 import com.exadel.etoolbox.querykit.core.models.search.SearchRequest;
 import com.exadel.etoolbox.querykit.core.services.modifiers.SearchItemConverterFactory;
 import com.exadel.etoolbox.querykit.core.services.modifiers.SearchItemFilterFactory;
@@ -15,9 +18,12 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
+import javax.jcr.RepositoryException;
 import javax.jcr.query.Query;
 import javax.jcr.query.qom.QueryObjectModel;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component(service = Executor.class)
 public class QomExecutor extends QueryBasedExecutor {
@@ -39,16 +45,36 @@ public class QomExecutor extends QueryBasedExecutor {
     }
 
     @Override
-    Query getBasicQuery(SearchRequest request) throws ConverterException {
-        return queryConverterService.convert(
+    Query getBasicQuery(SearchRequest request) throws ConverterException, RepositoryException {
+        QomAdapter qomAdapter = queryConverterService.convert(
                 request.getStatement(),
                 request.getResourceResolver(),
-                QomAdapter.class)
-                .getModel();
+                QomAdapter.class);
+        if (request.getUserParameters().isEmpty()) {
+            return qomAdapter.getModel();
+        }
+        if (!hasInterpolateableFields(qomAdapter.getConstraintAdapter(), request.getUserParameters().keySet())) {
+            return qomAdapter.getModel();
+        }
+        return qomAdapter.buildWith(request.getQueryManager().getQOMFactory(), request.getUserParameters()).getModel();
     }
 
     @Override
     ColumnCollection getColumnCollection(SearchRequest request, Query source) {
         return new ModifiableColumnCollection((QueryObjectModel) source);
+    }
+
+    private static boolean hasInterpolateableFields(ConstraintAdapter constraintAdapter, Set<String> candidateVariables) {
+        AtomicBoolean success = new AtomicBoolean();
+        constraintAdapter.visit(constraintAdapter1 -> {
+            if (!(constraintAdapter1 instanceof LiteralHolder)) {
+                return;
+            }
+            String value = ((LiteralHolder) constraintAdapter1).getLiteralValue();
+            if (candidateVariables.stream().anyMatch(candidate -> InterpolationHelper.isProcessable(value, candidate))) {
+                success.set(true);
+            }
+        });
+        return success.get();
     }
 }
--- core/src/main/java/com/exadel/etoolbox/querykit/core/services/executors/impl/SqlExecutor.java
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/services/executors/impl/SqlExecutor.java
@@ -1,6 +1,7 @@
 package com.exadel.etoolbox.querykit.core.services.executors.impl;
 
 import com.exadel.etoolbox.querykit.core.models.qom.QomAdapter;
+import com.exadel.etoolbox.querykit.core.models.qom.QomAdapterBundle;
 import com.exadel.etoolbox.querykit.core.models.qom.columns.ColumnCollection;
 import com.exadel.etoolbox.querykit.core.models.qom.columns.ModifiableColumnCollection;
 import com.exadel.etoolbox.querykit.core.models.search.SearchRequest;
@@ -9,6 +10,7 @@ import com.exadel.etoolbox.querykit.core.services.modifiers.SearchItemConverterF
 import com.exadel.etoolbox.querykit.core.services.modifiers.SearchItemFilterFactory;
 import com.exadel.etoolbox.querykit.core.services.executors.Executor;
 import com.exadel.etoolbox.querykit.core.services.converters.QueryConverter;
+import com.exadel.etoolbox.querykit.core.utils.Constants;
 import com.exadel.etoolbox.querykit.core.utils.ConverterException;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -30,7 +32,7 @@ public class SqlExecutor extends QueryBasedExecutor  {
     private static final String KEYWORD_SELECT = "select";
     private static final String KEYWORD_FROM = "from";
 
-    @Reference(target = "(converter.output=Qom)", policyOption = ReferencePolicyOption.GREEDY)
+    @Reference(target = "(converter.output=qom)", policyOption = ReferencePolicyOption.GREEDY)
     private QueryConverter queryConverterService;
 
     @Getter(AccessLevel.PACKAGE)
@@ -47,8 +49,17 @@ public class SqlExecutor extends QueryBasedExecutor  {
     }
 
     @Override
-    Query getBasicQuery(SearchRequest request) throws RepositoryException {
-        return request.getQueryManager().createQuery(request.getStatement(), Query.JCR_SQL2);
+    Query getBasicQuery(SearchRequest request) throws RepositoryException, ConverterException {
+        String statement = request.getStatement();
+
+        if (request.getUserParameters().size() > 0
+            && containsInterpolatableValues(request.getStatement(), request.getUserParameters().keySet())) {
+
+            statement = queryConverterService.convert(request.getStatement(), request.getResourceResolver(), QomAdapterBundle.class)
+                    .buildWith(request.getQueryManager().getQOMFactory(), request.getUserParameters())
+                    .toFormattedString();
+        }
+        return request.getQueryManager().createQuery(statement, Query.JCR_SQL2);
     }
 
     @Override
@@ -61,12 +72,24 @@ public class SqlExecutor extends QueryBasedExecutor  {
         return new ModifiableColumnCollection(qom);
     }
 
+    private static boolean containsInterpolatableValues(String statement, Set<String> variableNames) {
+        WordModel wordModel = new WordModel(statement);
+        for (String name : variableNames) {
+            boolean match = wordModel.hasToken(elt -> elt.startsWith("'") && elt.endsWith("'") && elt.contains("$" + name));
+            if (match) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     private static String prepareSelectExtract(String statement) {
         WordModel wordModel = new WordModel(statement);
         Set<String> selectors = new LinkedHashSet<>();
         WordModel selectorString = wordModel.extractBetween(KEYWORD_SELECT, KEYWORD_FROM);
         while (selectorString != null) {
-            selectors.addAll(selectorString.split(",")
+            selectors.addAll(selectorString.split(Constants.COMMA)
                     .stream()
                     .map(WordModel::toString)
                     .map(String::trim)
@@ -76,6 +99,9 @@ public class SqlExecutor extends QueryBasedExecutor  {
                     KEYWORD_FROM,
                     selectorString.getEndPosition());
         }
-        return String.format("SELECT %s FROM [nt:base]", String.join(",", selectors));
+        return String.format(
+                "SELECT %s FROM [%s]",
+                String.join(Constants.COMMA, selectors),
+                Constants.NODE_TYPE_PLACEHOLDER);
     }
 }
