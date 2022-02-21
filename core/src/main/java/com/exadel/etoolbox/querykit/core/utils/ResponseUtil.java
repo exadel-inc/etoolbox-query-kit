16212150a0db46e673aa845ff12e204b8f7fb1ce
Stepan Miakchilo <smiakchilo@exadel.com>
2022-02-21 10:50:32 +0300

    [Tech] Added utility methods for outputting text content

--- core/src/main/java/com/exadel/etoolbox/querykit/core/utils/ResponseUtil.java
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/utils/ResponseUtil.java
@@ -11,28 +11,41 @@ public class ResponseUtil {
 
     private static final String OUTPUT_MESSAGE_TEMPLATE = "{\"%s\":\"%s\"}";
 
-    public static void sendMessage(SlingHttpServletResponse response, String key, String value) throws IOException {
-        sendJson(response, getJsonMessage(key, value));
+    public static String getJsonMessage(String key, String value) {
+        return String.format(OUTPUT_MESSAGE_TEMPLATE, key, value);
     }
 
-    public static void sendError(SlingHttpServletResponse response, String value) throws IOException {
-        sendJson(response, HttpServletResponse.SC_BAD_REQUEST, getJsonMessage("error", "Invalid request"));
+    public static void sendString(SlingHttpServletResponse response, String content) throws IOException {
+        sendContent(response, HttpServletResponse.SC_OK, Constants.CONTENT_TYPE_TEXT, content);
+    }
+
+    public static void sendStringError(SlingHttpServletResponse response, String value) throws IOException {
+        sendContent(response, HttpServletResponse.SC_BAD_REQUEST, Constants.CONTENT_TYPE_TEXT, value);
     }
 
     public static void sendJson(SlingHttpServletResponse response, String content) throws IOException {
         sendJson(response, HttpServletResponse.SC_OK, content);
     }
 
+    public static void sendJsonMessage(SlingHttpServletResponse response, String key, String value) throws IOException {
+        sendJson(response, getJsonMessage(key, value));
+    }
+
+    public static void sendJsonError(SlingHttpServletResponse response, String value) throws IOException {
+        sendJson(response, HttpServletResponse.SC_BAD_REQUEST, getJsonMessage("error", value));
+    }
+
     private static void sendJson(SlingHttpServletResponse response, int status, String content) throws IOException {
-        response.setContentType(Constants.CONTENT_TYPE_JSON);
+        sendContent(response, status, Constants.CONTENT_TYPE_JSON, content);
+    }
+
+    private static void sendContent(SlingHttpServletResponse response, int status, String contentType, String content) throws IOException {
+        response.setContentType(contentType);
         response.setStatus(status);
         response.getWriter().println(content);
         response.getWriter().flush();
         response.getWriter().close();
-    }
 
-    public static String getJsonMessage(String key, String value) {
-        return String.format(OUTPUT_MESSAGE_TEMPLATE, key, value);
     }
 
 }
