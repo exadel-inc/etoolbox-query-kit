a1446118709a18c132879c31a4e1f17fd09feb71
Stepan Miakchilo <smiakchilo@exadel.com>
2022-02-21 11:00:16 +0300

    [Tech] Added a separate accessor for column names

--- core/src/main/java/com/exadel/etoolbox/querykit/core/models/qom/columns/ColumnCollection.java
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/models/qom/columns/ColumnCollection.java
@@ -1,13 +1,25 @@
 package com.exadel.etoolbox.querykit.core.models.qom.columns;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import javax.jcr.query.qom.Column;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface ColumnCollection {
 
     List<Column> getItems();
 
     List<String> getSelectors();
+
+    default List<String> getColumnNames() {
+        return CollectionUtils.emptyIfNull(getItems())
+                .stream()
+                .map(Column::getColumnName)
+                .filter(StringUtils::isNotBlank)
+                .collect(Collectors.toList());
+    }
 
     List<String> getPropertyNames();
 
