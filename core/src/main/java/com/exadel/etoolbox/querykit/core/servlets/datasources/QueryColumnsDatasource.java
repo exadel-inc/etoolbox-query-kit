8d3045303aabdd669466341c7f921b1841b8539d
Aliaksei Kaliada <akaliada@exadel.com>
2022-02-21 10:39:45 +0300

    [Tech] Created service datasources

---
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/servlets/datasources/QueryColumnsDatasource.java
@@ -0,0 +1,59 @@
+package com.exadel.etoolbox.querykit.core.servlets.datasources;
+
+import com.adobe.granite.ui.components.ds.DataSource;
+import com.adobe.granite.ui.components.ds.SimpleDataSource;
+import com.adobe.granite.ui.components.ds.ValueMapResource;
+import com.exadel.etoolbox.querykit.core.models.search.SearchRequest;
+import com.exadel.etoolbox.querykit.core.models.search.SearchResult;
+import com.exadel.etoolbox.querykit.core.services.query.QueryService;
+import com.exadel.etoolbox.querykit.core.utils.Constants;
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.Servlet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component(
+        service = Servlet.class,
+        property = {
+                "sling.servlet.resourceTypes=/apps/etoolbox-query-kit/datasources/queryColumns",
+                "sling.servlet.methods=" + HttpConstants.METHOD_GET
+        }
+)
+public class QueryColumnsDatasource extends SlingSafeMethodsServlet {
+
+    @Reference
+    private transient QueryService queryService;
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+        SearchRequest searchRequest = SearchRequest.from(request);
+        SearchResult searchResult = queryService.dryRun(searchRequest);
+
+        if (!searchResult.isSuccess() || searchResult.getColumns() == null) {
+            return;
+        }
+
+        List<Resource> resources = searchResult
+                .getColumns()
+                .getColumnNames()
+                .stream()
+                .map(name -> ImmutableMap.<String, Object>of(Constants.PROPERTY_JCR_TITLE, name))
+                .map(ValueMapDecorator::new)
+                .map(valueMap -> new ValueMapResource(request.getResourceResolver(), StringUtils.EMPTY, StringUtils.EMPTY, valueMap))
+                .collect(Collectors.toList());
+
+        if (!resources.isEmpty()) {
+            request.setAttribute(DataSource.class.getName(), new SimpleDataSource(resources.iterator()));
+        }
+    }
+}
--- core/src/main/java/com/exadel/etoolbox/querykit/core/utils/Constants.java
+++ core/src/main/java/com/exadel/etoolbox/querykit/core/utils/Constants.java
@@ -5,11 +5,18 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
 
+    public static final String ATTRIBUTE_ERROR_MESSAGE = "eqk-executionTime";
+    public static final String ATTRIBUTE_EXECUTION_TIME = "eqk-executionTime";
+    public static final String ATTRIBUTE_TOTAL = "eqk-total";
+
     public static final String CONTENT_TYPE_JSON = "application/json";
 
     public static final String NODE_DATASOURCE = "datasource";
 
     public static final String NODE_TYPE_PLACEHOLDER = "eqk:placeholder";
+
+    public static final String OPERATOR_IN = "in";
+    public static final String OPERATOR_UNION = "union";
 
     public static final String PROPERTY_JCR_PATH = "jcr:path";
     public static final String PROPERTY_JCR_TITLE = "jcr:title";
@@ -17,9 +24,6 @@ public class Constants {
     public static final String PROPERTY_TEXT = "text";
     public static final String PROPERTY_TYPE = "type";
     public static final String PROPERTY_VALUE = "value";
-
-    public static final String OPERATOR_IN = "in";
-    public static final String OPERATOR_UNION = "union";
 
     public static final String TITLE_PATH = "Path";
 
