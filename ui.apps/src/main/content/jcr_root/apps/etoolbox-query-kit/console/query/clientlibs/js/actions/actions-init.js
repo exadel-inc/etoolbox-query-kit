4e37ed009d8e9c10dd0bfee06c15665e833e442f
Stepan Miakchilo <smiakchilo@exadel.com>
2022-03-14 10:26:31 +0300

    [Tech] Added ability to call a previously registered action by name

---
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-query-kit/console/query/clientlibs/js/actions/actions-init.js
@@ -0,0 +1,22 @@
+(function (ns, $) {
+
+    'use strict';
+
+    $(document).ready(function () {
+
+        const registry = $(window).adaptTo('foundation-registry');
+
+        /** Retrieves a registered action by name and then runs it with the provided content and parameters */
+        ns.runAction = function (name, context, el, config, collection, selections) {
+            const target = registry.get('foundation.collection.action.action').find(function (action) {
+                return action.name === name;
+            });
+            if (!target) {
+                return;
+            }
+            target.handler.call(context, name, el, config, collection, selections);
+        };
+
+    });
+
+})(Granite.Eqk = (Granite.Eqk || {}), Granite.$);
